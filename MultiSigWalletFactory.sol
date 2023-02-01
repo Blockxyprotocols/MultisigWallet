1caf95a4663bad8c5b5c044e6ceb52447807dbf8
Blockxyprotocols <123859801+Blockxyprotocols@users.noreply.github.com>
2023-02-01 12:44:40 +0500

    Create MultiSigWalletFactory.sol

---
+++ MultiSigWalletFactory.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "./Factory.sol";
+import "./MultiSigWallet.sol";
+
+
+contract MultiSigWalletFactory is Factory 
+{
+function create(address[] memory _owners, uint _required) public returns (address multisig_wallet) 
+
+{
+multisig_wallet = address(new MultiSigWallet(_owners, _required));
+register(address(multisig_wallet));
+}
+
+}
