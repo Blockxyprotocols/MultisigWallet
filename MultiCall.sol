22975b8ef4ccc7bb14a3b6511f55a73c05ab2675
Blockxyprotocols <123859801+Blockxyprotocols@users.noreply.github.com>
2023-02-23 12:13:27 +0500

    Update MultiCall.sol

--- MultiCall.sol
+++ MultiCall.sol
@@ -1,15 +1,9 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v4.5.0) (utils/Multicall.sol)
 
 pragma solidity ^0.8.0;
 
 import "./Address.sol";
 
-/**
- * @dev Provides a function to batch together multiple calls in a single external call.
- *
- * _Available since v4.1._
- */
 abstract contract Multicall {
     /**
      * @dev Receives and executes a batch of function calls on this contract.
